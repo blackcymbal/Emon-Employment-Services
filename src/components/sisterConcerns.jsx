38375df8784070f968e003cd1c140feb9852ad45
Shahriar Mohsin Shohan <shohan.ndc@gmail.com>
2022-08-06 19:29:06 +0600

    first commit

---
+++ src/components/sisterConcerns.jsx
@@ -0,0 +1,53 @@
+export const SisterConcerns = (props) => {
+  return (
+    <div id='testimonials'>
+      <div className='container'>
+        <div className='section-title text-center'>
+          <h2>OUR SISTER CONCERNS</h2>
+        </div>
+        <div className='row'>
+          <ul>
+            <h1>1. Mercury & Co. mission</h1>
+            <p>
+              We have been in the service for the people of Bangladesh since
+              1990. With the experience gathered we took the initiative of
+              opening 'EMON EMPLOYMENT SERVICES' in 1990 .After that we started
+              Import-Export, Tourism Business, Manufacturing Garments and
+              agricultural products, Real Estate and many more. All the products
+              are imported directly from Malaysia, Thailand, Indonesia, United
+              Kingdom and United States of America. Before importing any product
+              our foreign office operatives check to make sure if the product’s
+              country of origin and standard according to the European
+              Standardization Policy is met. We strictly follow EU standards and
+              import nothing less. There is no third party involvement while
+              importing our products, which helps us to set the best offers
+              available in the country. Only selected, high-end products from
+              top of the line companies are imported for our valued customers.
+              Our goal is premium quality.
+            </p>
+            <p>
+              We understand the busy nature of our modern day customers. Hence
+              it became our mission to start MERCURY GROUP, serving specially
+              for their needs. Our customers can search through thousands of
+              famous brand products and services we provide from our ever
+              growing list in the website, with details about the products and
+              our services, to find the one suiting just their needs. Having
+              technology on our side we have devised our website to be as
+              accessible and easy as it would be fast. The high-end products are
+              neatly categorized for the ease of the customer, also saving their
+              valuable time. Stay Happy with MERCURY & CO., we look forward to
+              raising a revolution combining the gratification of the world with
+              the convenience of Internet. We promise to serve you with our full
+              effort.
+            </p>
+            <h1>2. Wahid & Emon Group Sdn Bhd</h1>
+            <h1>3. Fly Line Tours & Travels</h1>
+            <h1>4. Future Line Consultants</h1>
+            <h1>5. Wremon International</h1>
+            <h1>6. Navila International</h1>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/testimonials.jsx
@@ -0,0 +1,76 @@
+export const Testimonials = (props) => {
+  return (
+    <div id='testimonials'>
+      <div className='container'>
+        <div className='section-title text-center'>
+          <h2>OUR SISTER CONCERNS</h2>
+        </div>
+        <div className='row'>
+          <ul>
+            <h1>1. Mercury & Co. mission</h1>
+            <p>
+              We have been in the service for the people of Bangladesh since
+              1990. With the experience gathered we took the initiative of
+              opening 'EMON EMPLOYMENT SERVICES' in 1990 .After that we started
+              Import-Export, Tourism Business, Manufacturing Garments and
+              agricultural products, Real Estate and many more. All the products
+              are imported directly from Malaysia, Thailand, Indonesia, United
+              Kingdom and United States of America. Before importing any product
+              our foreign office operatives check to make sure if the product’s
+              country of origin and standard according to the European
+              Standardization Policy is met. We strictly follow EU standards and
+              import nothing less. There is no third party involvement while
+              importing our products, which helps us to set the best offers
+              available in the country. Only selected, high-end products from
+              top of the line companies are imported for our valued customers.
+              Our goal is premium quality.
+            </p>
+            <p>
+              We understand the busy nature of our modern day customers. Hence
+              it became our mission to start MERCURY GROUP, serving specially
+              for their needs. Our customers can search through thousands of
+              famous brand products and services we provide from our ever
+              growing list in the website, with details about the products and
+              our services, to find the one suiting just their needs. Having
+              technology on our side we have devised our website to be as
+              accessible and easy as it would be fast. The high-end products are
+              neatly categorized for the ease of the customer, also saving their
+              valuable time. Stay Happy with MERCURY & CO., we look forward to
+              raising a revolution combining the gratification of the world with
+              the convenience of Internet. We promise to serve you with our full
+              effort.
+            </p>
+            <h1>2. Wahid & Emon Group Sdn Bhd</h1>
+            <h1>3. Fly Line Tours & Travels</h1>
+            <h1>4. Future Line Consultants</h1>
+            <p>
+              Future Line Consultant's was established in 2015, It's a concern
+              of a renowned Wahid group of companies(Since 1985). Our main
+              concern is about the future of young generation. Our plan is to
+              help bangladeshi student's for building their future in the best
+              universities of malaysia. Now we already have 2 offices, 10
+              representatives, 2 experienced education counselors to advise
+              student's on courses and career opportunities according to their
+              educational background, interest, aptitude and aspirations. "We
+              reveal your latent talent" is our slogan, It literally reflects
+              our attitude and approach when providing services for students
+              building a bright, prosperous and best future.
+            </p>
+            <p>
+              Being a bridge across cultures and support for academic pursuit of
+              the younger generation is our mission. We at Future Line
+              consultant's offer overseas education consultancy. We are
+              committed to maintain high level of professionalism in our
+              service, free career counseling and information services,
+              registration and application, visa counseling and Interview
+              preparation services, travel and accommodation services and lift
+              off to an International career.
+            </p>
+            <h1>5. Wremon International</h1>
+            <h1>6. Navila International</h1>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
