e3948d04b9882e8142a0ee3acd6ce139ff719d55
Shahriar Mohsin Shohan <shohan.ndc@gmail.com>
2022-08-31 22:15:44 +0600

    Team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,20 +7,25 @@ export const Team = (props) => {
           <p>Our team combines creative, technical, and strategic talents.</p>
         </div>
         <div id='row'>
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
-                  <div className='thumbnail'>
-                    {" "}
-                    <img src={d.img} alt='...' className='team-img' />
-                    <div className='caption'>
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
+          <div className='portfolio-items'>
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.name}-${i}`}
+                    className='col-sm-6 col-md-4 col-lg-4'
+                  >
+                    <div className='thumbnail'>
+                      {" "}
+                      <img src={d.img} alt='...' className='team-img' />
+                      <div className='caption'>
+                        <h4>{d.name}</h4>
+                        <p>{d.job}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))
-            : "loading"}
+                ))
+              : "loading"}
+          </div>
         </div>
       </div>
     </div>
